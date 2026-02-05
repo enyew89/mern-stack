f3758c171ecc7b661afc29088e51e251e9452edc
Enyew Yirga <enyewyirga89@gmail.com>
2026-02-05 11:58:55 +0300

    done with controllers

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,15 @@
 import express from "express";
 import noteRouters from "./router/noteRouters.js";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 const app = express();
 
+connectDB();
+app.use(express.json());
 app.use("/api/notes", noteRouters);
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log("Server is running on port 4000");
 });
-
